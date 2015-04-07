a959a77bf6706ae41ced9842f86c19fd60734bb9
Andrea Edwards <andrea.edwards@colorado.edu>
2015-04-07 14:28:23 -0600

    Building method to retrieve specified gene sequence from genbank file

--- sequence/primer_design.py
+++ sequence/primer_design.py
@@ -28,6 +28,27 @@ def slice_sequence(gb_file, five_prime_res_site="BglII",
     # print start, end
     # print len(seq[start: end])
 
+def get_sequence(gb_file, gene_name):
+    """The get_sequence method retrieves the nucleotide sequence of a specfieid
+    gene from a genbank file.
+
+    Parameters
+    ----------
+    gb_file: string
+        String equivalent to the path pointing to relevant genbank file.
+    gene_name: string
+        Shorthand gene name such as "adhE" or "ldhA" that can be found in
+        genbank file under the gene category for particular CDS region
+
+    Returns
+    -------
+    string
+        The nucleotide sequence of indicated gene
+    """
+    from Bio import SeqIO
+    gb_file = open(gb_file, 'rU')
+
+
 
 def Tm_calc(seq, oligo_conc, salt_conc, RNA=0):
     import Bio
